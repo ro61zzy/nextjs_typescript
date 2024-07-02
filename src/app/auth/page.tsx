50c23b75a70ef3a8f5c95573a9f14974ccd3b299
rozzy <rosekaremeri@gmail.com>
2024-07-02 23:34:44 +0300

    creating a login application

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+
+export default function AuthRoute() {
+  return (
+    <Box>
+      <Typography>Please Sign In to Access Task Manager</Typography>
+      <Typography>Authentication Required</Typography>
+    </Box>
+  );
+}
