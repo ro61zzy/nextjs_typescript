9a3d2a675852f60e8a283779cfd272d7e26e936c
rozzy <rosekaremeri@gmail.com>
2024-07-03 13:21:48 +0300

    trying call back for specific domain login

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,28 +1,42 @@
-import { NextAuthOptions } from "next-auth"
-import { PrismaAdapter } from "@next-auth/prisma-adapter" //that works
+import { NextAuthOptions, User as NextAuthUser, Session as NextAuthSession } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./db";
 import GitHubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
 
-export const authOptions = {
-    adapter: PrismaAdapter(prisma) ,
-    //allow login or sign up with email or github
-    providers:[
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-          }),
-          EmailProvider({
-            server: {
-              host: process.env.EMAIL_SERVER_HOST,
-              port: process.env.EMAIL_SERVER_PORT,
-              auth: {
-                user: process.env.EMAIL_SERVER_USER,
-                pass: process.env.EMAIL_SERVER_PASSWORD
-              }
-            },
-            from: process.env.EMAIL_FROM
-          }),
-    ],
+const allowedDomains = ["sokofresh.co.ke", "anotherdomain.com"];
 
-} satisfies NextAuthOptions;
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        // user login with gmail
+        if (user.email && user.email.endsWith('sokofresh.co.ke')) {
+          return true
+        }
+
+        return false
+      } catch (error) {
+        return false
+      }
+    },
+}
+};
